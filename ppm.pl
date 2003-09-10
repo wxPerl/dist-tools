303484d36c3454241eb592bde87b8053ffa10b1c
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2003-09-10 17:45:23 +0000

    Correctly name archives at build time.   Handle slit Wx and Wx-dev PPM packages correctly.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/dist-tools/trunk@1118 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- ppm.pl
+++ ppm.pl
@@ -7,14 +7,23 @@ use FindBin;
 use lib "$FindBin::RealBin/lib";
 use DistConfig;
 use DistUtils;
+use Config;
 
 my $wxmsw_build = catdir( $temp_dir, 'wxMSW' );
 my $wxperl_build = catdir( $temp_dir, $wxperl_directory );
-my $wxperl_ppm_suffix = 'win32' . ( $wxperl_unicode ? '-u' : '' );
+my $wxperl_ppm_suffix = 'win32'
+                      . ( $wxperl_unicode ? '-u' : '' )
+                      . "-$Config{version}";
+my $wxperl_ppd = "Wx-${wxperl_version}.ppd";
 my $wxperl_ppm = "wxPerl-${wxperl_version}-" .
                  "wx-${wxwin_version}-${wxperl_ppm_suffix}.tar.gz";
 my $wxperl_ppm_archive = "Wx-${wxperl_version}-" .
-                 "wxmsw${wxwin_version}-${wxperl_ppm_suffix}.zip";
+          "wxmsw${wxwin_version}-${wxperl_ppm_suffix}.zip";
+my $wxperl_dev_ppd = "Wx-dev-${wxperl_version}.ppd";
+my $wxperl_dev_ppm = "wxPerl-dev-${wxperl_version}-" .
+                 "wx-${wxwin_version}-${wxperl_ppm_suffix}.tar.gz";
+my $wxperl_dev_ppm_archive = "Wx-dev-${wxperl_version}-" .
+          "wxmsw${wxwin_version}-${wxperl_ppm_suffix}.zip";
 
 check_files();
 unless( $ENV{NO_BUILD_WX} ) {
@@ -110,16 +119,19 @@ use Config;
 sub wxperl_ppm {
   my_chdir $wxperl_build;
 
-  my $real_arch = $] >= 5.008 ? $Config{archname} . '-5.8' : $Config{archname};
-
-  unlink 'Wx.ppd';
-  my_system 'dmake ppd ppmdist';
-  my_system "mv ${wxperl_directory}-ppm.tar.gz $wxperl_ppm";
-  # remove all dependencies, since ATM they are just compile-time,
-  # and Perl 5.8 PPM (probably correctly) chokes on them
-  my_system qq{perl -i.bak -p -e "s#^\\s*<DEPENDENCY.*/>\$##;s#<CODEBASE\\s+HREF=\\"\\"\\s+/>#<CODEBASE HREF=\\"${wxperl_ppm}\\" />#;s#\\"$Config{archname}\\"#\\"$real_arch\\"#" Wx.ppd};
+  my_system 'dmake ppmdist';
+  # fix archive name
+  for my $data ( [ $wxperl_ppd, $wxperl_ppm ],
+                 [ $wxperl_dev_ppd, $wxperl_dev_ppm ] ) {
+      my( $ppd, $pack ) = @$data;
+      my_system qq{perl -i.bak -p -e "s#<CODEBASE\\s+HREF=\\"\\S+\\"\\s+/>#<CODEBASE HREF=\\"${pack}\\" />#;" $ppd};
+  }
+  my_system "mv Wx-${wxperl_version}-ppm.tar.gz $wxperl_ppm";
   my_system "cp -f $data_dir/README.txt .";
-  my_system "zip -0 $distribution_dir/${wxperl_ppm_archive} $wxperl_ppm Wx.ppd README.txt";
+  my_system "zip -0 $distribution_dir/${wxperl_ppm_archive} $wxperl_ppm $wxperl_ppd README.txt";
+  my_system "mv Wx-dev-${wxperl_version}-ppm.tar.gz $wxperl_dev_ppm";
+  my_system "cp -f $data_dir/README.txt .";
+  my_system "zip -0 $distribution_dir/${wxperl_dev_ppm_archive} $wxperl_dev_ppm $wxperl_dev_ppd README.txt";
 }
 
 exit 0;
