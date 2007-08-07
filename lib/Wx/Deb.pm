7cca8e3c6d6f5ba37ae0c2b31ef5da5ad69e5a3a
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2007-08-07 21:33:06 +0000

    Remove some dead code.
    git-svn-id: http://svn.code.sf.net/p/wxperl/code/dist-tools/trunk@2121 2e9104e8-d5c8-4c90-bb67-bfd1792b7337

--- lib/Wx/Deb.pm
+++ lib/Wx/Deb.pm
@@ -60,13 +60,13 @@ sub set_options {
           $deb_release, $deb_arch );
 }
 
-sub build_wxwidgets {
+sub build_alien {
 }
 
 sub build_wxperl {
 }
 
-sub package_wxwidgets {
+sub package_alien {
 }
 
 sub package_wxperl {
@@ -157,85 +157,10 @@ EOT
 
 sub install_wxperl {
     return;
-
-    my $self = shift;
-    my $irpm = catfile( $self->_distconfig->remote_home,
-                        'buildarea', basename( $self->bin_rpm ) );
-
-    $self->_exec_string( sprintf <<'EOT', $irpm );
-#!/bin/sh
-
-# set -e
-set -x
-
-rpms=`rpm -q -a | grep Wx`
-if test "x$rpms" != "x"; then
-  sudo rpm -e $rpms
-fi
-
-sudo rpm -i %s
-EOT
 }
 
 sub build_submodules {
     return;
-
-    my( $self,  @modules ) = @_;
-    my $dc = $self->_distconfig;
-
-    $self->_exec_string( "mkdir -p cpan2rpm" );
-    $self->_put_file( $dc->data_dir . "/cpan2rpm/cpan2*",
-                      $dc->remote_home . "/cpan2rpm" );
-
-    foreach my $module ( @modules ) {
-        next if $module =~ m/Wx-ActiveX/;
-
-        my $package_src = $self->_distconfig->get_module_src( $module );
-        my $src_base = basename( $package_src );
-        my $package = $src_base; $package =~ s/\.tar\.gz|\.zip//;
-        my $package_nover = $package; $package_nover =~ s/\-[^-]+$//;
-        my $destination_dir = dirname( $package_src ) .
-          '/' . $dc->wxperl_version;
-
-        $self->_put_file( $package_src, $dc->remote_home );
-
-        my $ccache = $self->ccache;
-	my $xhost = $dc->xhost;
-
-        # build
-        $self->_exec_string( <<EOT );
-#!/bin/sh
-
-set -e
-set -x
-
-$ccache
-# \$functions
-
-export DISPLAY=$xhost
-
-sudo perl cpan2rpm/cpan2rpm --name $package_nover $src_base
-sudo rpmbuild -ba buildarea/SPECS/$package_nover.spec
-
-exit 0
-EOT
-
-        my $bin_rpm = "buildarea/RPMS/*/perl-$package-1.i686.rpm";
-        my $src_rpm = "buildarea/SRPMS/perl-$package-1.src.rpm";
-
-#        die "something went wrong while building ($bin_rpm) ($src_rpm)"
-#          unless defined( $bin_rpm ) && defined( $src_rpm ) &&
-#            -f $bin_rpm && -f $src_rpm;
-
-        my $src_final_rpm = basename( $src_rpm );
-        my $bin_final_rpm = basename( $bin_rpm );
-        my $wxwin_version = $dc->wxwin_version;
-        my $wxperl_version = $dc->wxperl_version;
-        $bin_final_rpm =~ s/(\.[^\.]+\.rpm)$/_wxperl${wxperl_version}_wxgtk${wxwin_version}${1}/;
-
-        $self->_get_file( $src_rpm, "$destination_dir/$src_final_rpm" );
-        $self->_get_file( $bin_rpm, "$destination_dir/$bin_final_rpm" );
-    }
 }
 
 sub make_dist {
